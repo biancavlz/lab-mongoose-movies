f43612af6a515edc9788342f488a461ebf56d1cc
Bianca <ing_biank@hotmail.com>
2019-04-15 23:06:53 +0200

    Adds new/create celebrity

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -19,6 +19,23 @@ router.get('/celebrities/', (req, res) => {
   })
 })
 
+router.get('/celebrities/new', (req, res) => {
+  res.render('./celebrities/celebrity-new');
+});
+
+router.post('/celebrities/new', (req, res) => {
+  const { name, occupation, catchPhrase} = req.body;
+
+  Celebrity.create({ name, occupation, catchPhrase})
+    .then(() => {
+      console.log('Celebrity successfully created');
+      res.redirect('/celebrities/');
+    })
+    .catch(err => {
+      console.error('Error while creating celebrity', err);
+  });
+});
+
 router.get('/celebrities/:celebrityId', (req, res) => {
   const _id = req.params.celebrityId;
   Celebrity.findOne({ _id })
@@ -30,5 +47,8 @@ router.get('/celebrities/:celebrityId', (req, res) => {
       console.error('Error while retrieving celebrity with id ' + _id, err);
   });
 });
+
+
+
 
 module.exports = router;
